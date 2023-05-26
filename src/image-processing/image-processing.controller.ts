e70ebaf0ec02e39b3ecc1a8c632d58405d58e817
Armando Barboza <armando.barboza.ojeda@gmail.com>
2023-05-25 23:44:19 -0400

    Adding bearer token guard

--- src/image-processing/image-processing.controller.ts
+++ src/image-processing/image-processing.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { ImageUtilsService } from './image-utils.service';
 import { FileExtension } from 'src/custom-exceptions/file-extension';
 import { ImageUploadDto } from 'src/models/image-upload-dto';
+import { JwtRefreshAuthGuardGuard } from 'src/jwt-refresh-auth-guard/jwt-refresh-auth-guard.guard';
 
 @Controller('image-processing')
 export class ImageProcessingController {
     constructor(private imageSvc: ImageUtilsService) { }
 
     @Post('upload')
+    @UseGuards(JwtRefreshAuthGuardGuard)
     @UseInterceptors(FileInterceptor('image'))
     async uploadImage(@UploadedFile() @FileExtension() dto: ImageUploadDto) {
         return await this.imageSvc.compress(dto.image);
